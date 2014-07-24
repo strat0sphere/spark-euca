1ef07dbd3772cac56f5b5ab3b6ba4f5b68b1d2d7
Stratos Dimopoulos <stratos@sphere.local>
2014-07-24 10:17:25 -0700

    cleanup

--- prepare-slaves-centos.sh
+++ prepare-slaves-centos.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
-echo 'exclude=pam*' >> /etc/yum.conf;yum update
+#echo 'exclude=pam*' >> /etc/yum.conf;yum update
+exclude=pam; yum update
 yum -y install wget
 yum -y install git
 yum -y java-1.7.0-openjdk
-#wget https://archive.apache.org/dist/hadoop/core/hadoop-1.0.4/hadoop-1.0.4.tar.gz
 mv /usr/lib/jvm/java-1.7.0-openjdk-1.7.0.65.x86_64/ /usr/lib/jvm/java-1.7.0/
 wget http://downloads.typesafe.com/scala/2.11.1/scala-2.11.1.tgz
 tar xvf scala-2.11.1.tgz
 mv scala-2.11.1 scala
-#echo export http_proxy=http_proxy_option >> ~/.bash_profile
-#echo 'export JAVA_HOME=/usr/lib/jvm/java-1.7.0'  >> ~/.bash_profile
-#echo 'export SCALA_HOME=/root/scala' >> ~/.bash_profile
-#echo 'export PATH=$PATH:/root/scala/bin:/usr/lib/jvm/java-1.7.0/bin' >> ~/.bash_profile
-#cp /home/ubuntu/.bash_profile /root/.bash_profile
-#source /root/.bash_profile
-#source /root/.profile
--- prepare-slaves-ubuntu.sh
+++ prepare-slaves-ubuntu.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 apt-get update
 apt-get --yes --force-yes install git
 apt-get --yes --force-yes install openjdk-7-jdk
@@ -5,12 +6,3 @@ mv /usr/lib/jvm/java-7-openjdk-amd64 /usr/lib/jvm/java-1.7.0
 wget http://downloads.typesafe.com/scala/2.11.1/scala-2.11.1.tgz
 tar xvf scala-2.11.1.tgz
 mv scala-2.11.1 scala
-
-#wget https://archive.apache.org/dist/hadoop/core/hadoop-1.0.4/hadoop-1.0.4.tar.gz
-#echo export http_proxy=http_proxy_option >> ~/.bash_profile
-#echo 'export JAVA_HOME=/usr/lib/jvm/java-1.7.0'  >> ~/.bash_profile
-#echo 'export SCALA_HOME=/root/scala' >> ~/.bash_profile
-#echo 'export PATH=$PATH:/root/scala/bin:/usr/lib/jvm/java-1.7.0/bin' >> ~/.bash_profile
-#cp /home/ubuntu/.bash_profile /root/.bash_profile
-#source /root/.bash_profile
-#source /root/.profile
