613b42017a3f5ea26c6fb247c5000ccf8b187f9f
Stratos Dimopoulos <stratos.dimopoulos@gmail.com>
2014-09-15 02:02:02 -0700

    draft modifications for mesos 0.20.0 - just backing up - not ready to use

--- mesos/config-master.sh
+++ mesos/config-master.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 #makesure port 5050 is open - euca-authorize -P tcp -p 5050 -s 0.0.0.0/0 stratos
-export LD_LIBRARY_PATH=/root/mesos/build/src/.libs/ #   Fixes error while loading shared libraries: libmesos--.xx.xx.so: cannot open shared object file: No such file or directory
-nohup /usr/local/sbin/mesos-master --cluster=ucsb --log_dir=/mnt/mesos-logs start </dev/null >/dev/null 2>&1 &
+#export LD_LIBRARY_PATH=/root/mesos/build/src/.libs/ #   Fixes error while loading shared libraries: libmesos--.xx.xx.so: cannot open shared object file: No such file or directory
+export LD_LIBRARY_PATH=/root/mesos-$MESOS_VERSION/build/src/.libs/
+#nohup /root/mesos-installation/sbin/mesos-master --cluster=mesos-2node-cluster --log_dir=/mnt/mesos-logs start </dev/null >/dev/null 2>&1 &
+
+
+
+#with Zookeeper for mesos 0.18.1
+#nohup /root/mesos-installation/sbin/mesos-master --cluster=mesos-2node-cluster --log_dir=/mnt/mesos-logs --zk=zk://10.2.7.122:2181/mesos start </dev/null >/dev/null 2>&1 &
+
+#With Zookeeper for mesos 0.20.0
+nohup /root/mesos-installation/sbin/mesos-master --cluster=mesos-spark-hadoop-cdh5-cluster --log_dir=/mnt/mesos-logs --zk=zk://10.2.24.25:2181/mesos --work_dir=/mnt/mesos-work-dir/ --quorum=1 start </dev/null >/dev/null 2>&1 &
 
 #--log_dire=VALUE --> Default /etc/mesos
--- mesos/config-slave.sh
+++ mesos/config-slave.sh
@@ -1,5 +1,15 @@
 #ATTENTION: User the IP used on the file /usr/local/var/mesos/deploy/slaves  and .../masters
-nohup /usr/local/sbin/mesos-slave --log_dir=/mnt/mesos-logs --master=euca-128-111-179-167.eucalyptus.race.cs.ucsb.edu:5050 </dev/null >/dev/null 2>&1 &
+#nohup /root/mesos-installation/sbin/mesos-slave --log_dir=/mnt/mesos-logs --master=euca-128-111-179-167.eucalyptus.race.cs.ucsb.edu:5050 </dev/null >/dev/null 2>&1 &
+export LD_LIBRARY_PATH=/root/mesos-$MESOS_VERSION/build/src/.libs/
+#with zookeper
+nohup /root/mesos-installation/sbin/mesos-slave --log_dir=/mnt/mesos-logs --master=zk://10.2.7.122:2181/mesos </dev/null >/dev/null 2>&1 &
+
+#HACK: Modify the script called internally from mesos to target hadoop
+#This is not needed if a cloudera version is used
+#TODO Create the command hadoop to target on the existing hdfs installation
+#cat "#/bin/bash" >> /usr/bin/hadoop
+#cat 'exec /root/ephemeral-hdfs/bin/hadoop "$@"'>> /usr/bin/hadoop
+
 ## OR: nohup /usr/local/sbin/mesos-slave start --log_dir=/mnt/mesos-logs --master=10.2.175.248:5050
 
 
@@ -10,3 +20,6 @@ nohup /usr/local/sbin/mesos-slave --log_dir=/mnt/mesos-logs --master=euca-128-11
 #--isolation=VALUE #Isolation mechanism, may be one of: process, cgroups (default: process)
 
 #--work_dir=VALUE                           #Where to place framework work directories (default: /tmp/mesos)
+
+#For Mesos 0.20.0 with Zookeeper
+nohup /root/mesos-installation/sbin/mesos-slave --log_dir=/mnt/mesos-logs --master=zk://10.2.24.25:2181/mesos </dev/null >/dev/null 2>&1 &
