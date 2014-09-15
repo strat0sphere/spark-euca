471ba4b86715700d63fee1c55f2ce1ec84454523
Stratos Dimopoulos <stratos.dimopoulos@gmail.com>
2014-09-15 02:12:08 -0700

    installing java 6 - necessary to create spark assembly withou incompatibilities

--- mesos/setup.sh
+++ mesos/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt-get update
 apt-get --yes --force-yes install build-essential
-#apt-get --yes --force-yes install openjdk-6-jdk
+apt-get --yes --force-yes install openjdk-6-jdk
 apt-get --yes --force-yes install python-dev python-boto
 apt-get --yes --force-yes install libcurl4-nss-dev
 apt-get --yes --force-yes install libsasl2-dev
@@ -34,7 +34,7 @@ make
 make install
 
 # Run test suite -- Also builds example frameworks
-#make check #Run make check at the end because some tests fail
+make check #Run make check at the end because some tests fail (VERSION 0.18.1)
 
 /root/spark-euca/copy-dir /root/mesos-$MESOS_SETUP_VERSION
 /root/spark-euca/copy-dir /root/mesos-installation/
