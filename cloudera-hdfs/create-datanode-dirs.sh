eed94376e40b219878dafea07223eb69b814f646
Stratos Dimopoulos <stratos.dimopoulos@gmail.com>
2014-10-01 14:55:49 -0700

    create hdfs directories

---
+++ cloudera-hdfs/create-datanode-dirs.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+mkdir -p /mnt/cloudera-hdfs/1/dfs/dn /mnt/cloudera-hdfs/2/dfs/dn /mnt/cloudera-hdfs/3/dfs/dn /mnt/cloudera-hdfs/4/dfs/dn
+chown -R hdfs:hdfs /mnt/cloudera-hdfs/1/dfs/dn /mnt/cloudera-hdfs/2/dfs/dn /mnt/cloudera-hdfs/3/dfs/dn /mnt/cloudera-hdfs/4/dfs/dn
