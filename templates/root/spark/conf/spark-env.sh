3b225badfde2fddcd673711f1d97b34c54bef6e3
Stratos Dimopoulos <stratos.dimopoulos@gmail.com>
2014-10-01 13:56:43 -0700

    templates for hdfs, mesos, spark and mapred

--- templates/root/spark/conf/spark-env.sh
+++ templates/root/spark/conf/spark-env.sh
@@ -1,21 +1,7 @@
 #!/usr/bin/env bash
 
-export SPARK_LOCAL_DIRS="{{spark_local_dirs}}"
+# This file is sourced when running various Spark programs.
+# Copy it as spark-env.sh and edit that to configure Spark for your site.
 
-# Standalone cluster options
-export SPARK_MASTER_OPTS="{{spark_master_opts}}"
-export SPARK_WORKER_INSTANCES={{spark_worker_instances}}
-export SPARK_WORKER_CORES={{spark_worker_cores}}
-
-export HADOOP_HOME="/root/ephemeral-hdfs"
-export SPARK_MASTER_IP={{active_master}}
-export MASTER=`cat /root/spark-euca/cluster-url`
-
-export SPARK_SUBMIT_LIBRARY_PATH="$SPARK_SUBMIT_LIBRARY_PATH:/root/ephemeral-hdfs/lib/native/"
-export SPARK_SUBMIT_CLASSPATH="$SPARK_CLASSPATH:$SPARK_SUBMIT_CLASSPATH:/root/ephemeral-hdfs/conf"
-
-# Bind Spark's web UIs to this machine's public EC2 hostname:
-export SPARK_PUBLIC_DNS=`wget -q -O - http://169.254.169.254/latest/meta-data/public-hostname`
-
-# Set a high ulimit for large shuffles
-ulimit -n 1000000
+export MESOS_NATIVE_LIBRARY=/root/mesos-installation/lib/libmesos.so
+export SPARK_EXECUTOR_URI=hdfs://{{active_master}}:9000/spar
