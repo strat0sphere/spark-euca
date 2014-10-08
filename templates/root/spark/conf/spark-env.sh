b6e50e10930fac0b1eeadef6484102502a3c8f7c
Stratos Dimopoulos <stratos.dimopoulos@gmail.com>
2014-10-07 17:53:57 -0700

    bug fix

--- templates/root/spark/conf/spark-env.sh
+++ templates/root/spark/conf/spark-env.sh
@@ -4,4 +4,5 @@
 # Copy it as spark-env.sh and edit that to configure Spark for your site.
 
 export MESOS_NATIVE_LIBRARY=/root/mesos-installation/lib/libmesos.so
-export SPARK_EXECUTOR_URI=hdfs://{{active_master_private}}:9000/spar
+export SPARK_EXECUTOR_URI=hdfs://{{active_master_private}}:9000/spark-1.1.0-bin-2.3.0.tgz
+export HADOOP_CONF_DIR=/etc/hadoop/conf.mesos-cluster/
