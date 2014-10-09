c78a6963f4cae9ebf4fa1f20aac4f889e664a609
Stratos Dimopoulos <stratos.dimopoulos@gmail.com>
2014-10-08 19:44:26 -0700

    changing temp files owner

--- hadoop-on-mesos/setup-test.sh
+++ hadoop-on-mesos/setup-test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-su mapred
 echo "I love racelab" > /tmp/file0
 echo "Do you love racelab?" > /tmp/file1
+chown hdfs:hadoop /tmp/file0
+chown hdfs:hadoop /tmp/file1
+
 hadoop fs -mkdir -p /user/foo/data
 hadoop fs -put /tmp/file? /user/foo/data
-
-su root
