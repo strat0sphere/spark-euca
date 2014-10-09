7ff0a80688a23fb5b902c56410bb18c640a27c58
Stratos Dimopoulos <stratos.dimopoulos@gmail.com>
2014-10-08 18:13:55 -0700

    bug fix

--- hadoop-on-mesos/init.sh
+++ hadoop-on-mesos/init.sh
@@ -2,11 +2,13 @@
 
 mkdir /executor_tars
 wget -P /executor_tars http://php.cs.ucsb.edu/spark-related-packages/executor_tars/hadoop-2.3.0-cdh5.1.2-mesos.0.20.tar.gz
+
+chown -R /executor_tars
+
 echo "Putting hadoop-2.3.0-cdh5.1.2-mesos.0.20.tar.gz to HDFS..."
 #/executor_tars directory already exists on the emi
 hadoop fs -put /executor_tars/hadoop-2.3.0-cdh5.1.2-mesos.0.20.tar.gz /
 hadoop fs -ls /
 
-chown -R /executor_tars
 #delete to save some space if necessary
 rm /executor_tars/hadoop-2.3.0-cdh5.1.2-mesos.0.20.tar.gz
--- spark-on-mesos/init.sh
+++ spark-on-mesos/init.sh
@@ -3,11 +3,12 @@
 mkdir /executor_tars
 wget -P /executor_tars http://php.cs.ucsb.edu/spark-related-packages/executor_tars/spark-1.1.0-bin-2.3.0.tgz
 
+chown -R /executor_tars
+
 echo "Putting spark-1.1.0-bin-2.3.0.tgz to HDFS..."
 #/executor_tars directory already exists on the emi
 hadoop fs -put /executor_tars/spark-1.1.0-bin-2.3.0.tgz /
 hadoop fs -ls /
 
-chown -R /executor_tars
 #delete to save some space if necessary
 rm /executor_tars/spark-1.1.0-bin-2.3.0.tgz
