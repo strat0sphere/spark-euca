08ca9865942ff4894789df5b899a4420110b7051
Stratos Dimopoulos <stratos.dimopoulos@gmail.com>
2014-10-08 20:18:06 -0700

    bug fixes

--- spark-on-mesos/run-test.sh
+++ spark-on-mesos/run-test.sh
@@ -1,3 +1,3 @@
 #bin/bash
 
-root/spark/bin/spark-submit --class WordCount3 --master mesos://zk://$ACTIVE_MASTER_PRIVATE/mesos ~/test-code/simple-project_2.10-1.0.jar $ACTIVE_MASTER_PRIVATE
+/root/spark/bin/spark-submit --class WordCount3 --master mesos://zk://$ACTIVE_MASTER_PRIVATE/mesos ~/test-code/simple-project_2.10-1.0.jar $ACTIVE_MASTER_PRIVATE
