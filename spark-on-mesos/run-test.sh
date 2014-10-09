98670b24dc75378a3f985eb9e11b867e68fdbb10
Stratos Dimopoulos <stratos.dimopoulos@gmail.com>
2014-10-09 12:10:35 -0700

    getting the executor

--- spark-on-mesos/run-test.sh
+++ spark-on-mesos/run-test.sh
@@ -1,3 +1,4 @@
 #bin/bash
 
+wget -P /executor_tars http://php.cs.ucsb.edu/spark-related-packages/executor_tars/spark-1.1.0-bin-2.3.0.tgz
 /root/spark/bin/spark-submit --class WordCount3 --master mesos://zk://$ACTIVE_MASTER_PRIVATE/mesos ~/test-code/simple-project_2.10-1.0.jar $ACTIVE_MASTER_PRIVATE
