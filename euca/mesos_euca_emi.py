27af09a5e72bd40b1b77bbe4c0d3d40c7e0dc28e
Stratos Dimopoulos <stratos.dimopoulos@gmail.com>
2014-10-08 18:39:17 -0700

    removing mesos from modules

--- euca/mesos_euca_emi.py
+++ euca/mesos_euca_emi.py
@@ -435,7 +435,7 @@ def setup_cluster(conn, master_nodes, slave_nodes, zoo_nodes, opts, deploy_ssh_k
 
   #modules = ['spark', 'shark', 'ephemeral-hdfs', 'persistent-hdfs', 'mapreduce', 'spark-standalone', 'tachyon']
 
-  modules = ['mesos', "hadoop-on-mesos", "spak-on-mesos"]
+  modules = ["hadoop-on-mesos", "spak-on-mesos"]
 
   ssh(master, opts, "rm -rf spark-euca && git clone -b mesos-emi https://github.com/strat0sphere/spark-euca.git")
 
