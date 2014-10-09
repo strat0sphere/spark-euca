5d72f326f12841ae7d9887c3cf4eca9b00f1038d
Stratos Dimopoulos <stratos.dimopoulos@gmail.com>
2014-10-08 18:10:42 -0700

    getting executors from remote server

--- spark-on-mesos/init.sh
+++ spark-on-mesos/init.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+mkdir /executor_tars
+wget -P /executor_tars http://php.cs.ucsb.edu/spark-related-packages/executor_tars/spark-1.1.0-bin-2.3.0.tgz
+
 echo "Putting spark-1.1.0-bin-2.3.0.tgz to HDFS..."
 #/executor_tars directory already exists on the emi
 hadoop fs -put /executor_tars/spark-1.1.0-bin-2.3.0.tgz /
 hadoop fs -ls /
 
+chown -R /executor_tars
 #delete to save some space if necessary
 rm /executor_tars/spark-1.1.0-bin-2.3.0.tgz
