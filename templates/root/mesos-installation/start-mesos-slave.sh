184eb27796adc5ec0e9bba5ee7a502c748ca99ea
Stratos Dimopoulos <stratos.dimopoulos@gmail.com>
2014-10-12 14:00:19 -0700

    tamplate var to lowercase

--- templates/root/mesos-installation/start-mesos-slave.sh
+++ templates/root/mesos-installation/start-mesos-slave.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-nohup /root/mesos-installation/sbin/mesos-slave --log_dir=/mnt/mesos-logs --work_dir=/mnt/mesos-work-dir/ --master=zk://{{ACTIVE_MASTER_PRIVATE}}:2181/mesos </dev/null >/dev/null 2>&1 &
+nohup /root/mesos-installation/sbin/mesos-slave --log_dir=/mnt/mesos-logs --work_dir=/mnt/mesos-work-dir/ --master=zk://{{active_master_private}}:2181/mesos </dev/null >/dev/null 2>&1 &
