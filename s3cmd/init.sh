e9a05750f0c4f3018cf193fe25e04d4bb27bec76
Stratos Dimopoulos <stratos.dimopoulos@gmail.com>
2014-10-16 23:24:36 -0700

    deleting code to check if s3cmd exists

--- s3cmd/init.sh
+++ s3cmd/init.sh
@@ -2,11 +2,6 @@
 
 pushd /root
 
-if [ -d "/etc/s3cmd" ]; then
-echo "c3cmd seems to be installed. Exiting."
-return
-fi
-
 git clone https://github.com/eucalyptus/s3cmd
 cd s3cmd
 apt-get install python-dateutil
