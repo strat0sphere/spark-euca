51db3dc77ebac8b94322f8548bdaba4fccd5f8cf
Stratos Dimopoulos <stratos.dimopoulos@gmail.com>
2014-10-16 23:07:14 -0700

    sh --> bash

--- s3cmd/init.sh
+++ s3cmd/init.sh
@@ -1,12 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
 pushd /root
 
-if [ -d "s3cmd" ]; then
+if [ -d "/etc/s3cmd" ]; then
 echo "c3cmd seems to be installed. Exiting."
 return
 fi
 
-git clone https://github.com/s3tools/s3cmd.git
+git clone https://github.com/eucalyptus/s3cmd
+cd s3cmd
+apt-get install python-dateutil
+python setup.py install
+cd ..
 
 popd
