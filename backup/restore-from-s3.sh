b6797782516f4030d5e94797e7870ebc51cc02c4
Stratos Dimopoulos <stratos.dimopoulos@gmail.com>
2014-10-16 23:20:05 -0700

    adding echo messages

--- backup/restore-from-s3.sh
+++ backup/restore-from-s3.sh
@@ -5,10 +5,15 @@ mkdir -p /mnt/hdfs-backup
 chown -R hdfs:hadoop /mnt/hdfs-backup
 
 
-
+echo "Geting files from S3..."
 #Get the file from S3
 s3cmd -c /root/s3cmd/s3cfg get --recursive --disable-multipart s3://$CLUSTER_NAME/hdfs-backup/ /mnt/hdfs-backup/
 
+echo "Putting files on HDFS..."
 hadoop fs -put /mnt/hdfs-backup/* /
+
+rm -rf /mnt/hdfs-backup #delete previous backups
+mkdir -p /mnt/hdfs-backup
+chown -R hdfs:hadoop /mnt/hdfs-backup
 
 #  --delete-after-fetch  Delete remote objects after fetching to local file
