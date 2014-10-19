86961ff3d11e7b08ecc5c0e8445086361932b9ea
Stratos Dimopoulos <stratos.dimopoulos@gmail.com>
2014-10-19 00:24:17 -0700

    add debuggin messages

--- backup/init.sh
+++ backup/init.sh
@@ -3,7 +3,9 @@
 pushd /root
 
 if [[ "$RESTORE" == "False" ]]; then
+    echo "Creating bucket S3://$CLUSTER_NAME ..."
     s3cmd -c /etc/s3cmd/s3cfg mb s3://$CLUSTER_NAME #create the bucket
+    echo "Done!"
 fi
 
 popd
--- backup/restore-from-s3.sh
+++ backup/restore-from-s3.sh
@@ -8,9 +8,11 @@ chown -R hdfs:hadoop /mnt/hdfs-backup
 echo "Geting files from S3..."
 #Get the file from S3
 s3cmd -c /etc/s3cmd/s3cfg get --recursive --disable-multipart s3://$CLUSTER_NAME/hdfs-backup/ /mnt/hdfs-backup/
+echo "Done!"
 
 echo "Putting files on HDFS..."
 sudo -u hdfs hadoop fs -put /mnt/hdfs-backup/* /
+echo "Done!"
 
 rm -rf /mnt/hdfs-backup #delete previous backups
 mkdir -p /mnt/hdfs-backup
