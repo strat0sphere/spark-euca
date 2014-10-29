1a0b1a361bb006333983e6ed188f7d89ccff293d
Stratos Dimopoulos <stratos.dimopoulos@gmail.com>
2014-10-29 16:43:33 -0700

    remove old dir

--- mesos/init.sh
+++ mesos/init.sh
@@ -6,6 +6,11 @@ if [ -d "mesos-$MESOS_SETUP_VERSION" ]; then
 echo "Mesos seems to be installed. Exiting."
 return 0
 fi
+
+#Remove old installed version
+echo "Removing old mesos-installation"
+rm -rf mesos-installation
+
 echo "Initializing $MESOS_SETUP_VERSION"
 MESOS_SETUP_VERSION=$MESOS_SETUP_VERSION
 wget http://archive.apache.org/dist/mesos/$MESOS_SETUP_VERSION/mesos-$MESOS_SETUP_VERSION.tar.gz
