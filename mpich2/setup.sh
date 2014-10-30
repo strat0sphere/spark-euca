0afa62039b43cc47acea5cadef09c48a119f9133
Stratos Dimopoulos <stratos.dimopoulos@gmail.com>
2014-10-29 17:07:20 -0700

    test code and corrections

--- mpich2/setup.sh
+++ mpich2/setup.sh
@@ -18,4 +18,7 @@ chmod 600 /etc/mpd.conf
 #Copy /mpich2-install to all machines
 /root/spark-euca/copy-dir /root/mpich2-install
 
+#Boot mpd ring
+mpdboot --file=/etc/mpd.hosts --verbose -n 3
+
 popd /root
--- mpich2/test.sh
+++ mpich2/test.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
 #Test installation
+
+echo "Testing if MPI commands are configured correctly... "
+which mpiexec
+which mpd
+which mpirun
+
+echo "Running mpdtrace to check if mpd ring is working... "
+#mpd &
+mpdtrace
+#mpdallexit
+
+echo "Compiling mpi helloworld code"
 #Download helloworld code
 mpicc -o helloworld helloworld.c
 
-/mnt/mesos/mpi/mpiexec-mesos.py zk://euca-10-2-248-74.eucalyptus.internal:2181/mesos ./helloworld
+/root/mesos/mpi/mpiexec-mesos.in zk://$ACTIVE_MASTER_PRIVATE:2181/mesos ./helloworld
