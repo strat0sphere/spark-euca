39da33777eb186338c78348f53578b587fcc880a
Stratos Dimopoulos <stratos.dimopoulos@gmail.com>
2014-11-02 13:57:29 -0800

    copy executable to slaves

--- mpich2/test.sh
+++ mpich2/test.sh
@@ -16,4 +16,7 @@ echo "Compiling mpi helloworld code"
 #Download helloworld code
 mpicc -o helloworld helloworld.c
 
+#Copy executable to slaves
+#/root/spark-euca/copy-dir helloworld
+
 /root/mesos/mpi/mpiexec-mesos.in zk://$ACTIVE_MASTER_PRIVATE:2181/mesos ./helloworld
