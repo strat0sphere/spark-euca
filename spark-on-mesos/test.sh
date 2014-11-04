d6ceafef5c0194cc9e78a614a87c41f5989bb5d7
Stratos Dimopoulos <stratos.dimopoulos@gmail.com>
2014-11-03 18:59:52 -0800

    geting test code from server

--- spark-on-mesos/test.sh
+++ spark-on-mesos/test.sh
@@ -9,6 +9,13 @@ hadoop fs -mkdir -p /user/foo/data
 hadoop fs -put /tmp/file0 /user/foo/data
 
 wget -P /executor_tars http://php.cs.ucsb.edu/spark-related-packages/executor_tars/spark-1.1.0-bin-2.3.0.tgz
+
+
+
+wget -P /root/test-code http://php.cs.ucsb.edu/spark-related-packages/test-code/simple-project_2.10-1.0.jar
+
+hadoop fs -put /executor_tars/spark-1.1.0-bin-2.3.0.tgz /
+
 /root/spark/bin/spark-submit --class WordCount3 --master mesos://zk://$ACTIVE_MASTER_PRIVATE/mesos ~/test-code/simple-project_2.10-1.0.jar $ACTIVE_MASTER_PRIVATE
 
 popd /root
