94ee0e2dea631788e2ad927bdad2ccedce3e845a
Stratos Dimopoulos <stratos.dimopoulos@gmail.com>
2014-11-05 17:35:48 -0800

    tests updated

--- hadoop-on-mesos/test.sh
+++ hadoop-on-mesos/test.sh
@@ -8,6 +8,8 @@ chown hdfs:hadoop /tmp/file1
 hadoop fs -mkdir -p /user/foo/data
 hadoop fs -put /tmp/file? /user/foo/data
 
+echo "Executing: hadoop jar /usr/lib/hadoop-0.20-mapreduce/hadoop-examples-2.3.0-mr1-cdh5.1.2.jar wordcount /user/foo/data /user/foo/out"
+
 hadoop jar /usr/lib/hadoop-0.20-mapreduce/hadoop-examples-2.3.0-mr1-cdh5.1.2.jar wordcount /user/foo/data /user/foo/out
 hadoop fs -ls /user/foo/out
 hadoop fs -cat /user/foo/out/part*
--- spark-on-mesos/test.sh
+++ spark-on-mesos/test.sh
@@ -8,14 +8,12 @@ chown hdfs:hadoop /tmp/file0
 hadoop fs -mkdir -p /user/foo/data
 hadoop fs -put /tmp/file0 /user/foo/data
 
-wget -P /executor_tars http://php.cs.ucsb.edu/spark-related-packages/executor_tars/spark-1.1.0-bin-2.3.0.tgz
 
-
+echo "Getting sample test code from server..."
 rm /root/test-code/simple-project_2.10-1.0.jar
 wget -P /root/test-code http://php.cs.ucsb.edu/spark-related-packages/test-code/simple-project_2.10-1.0.jar
 
-hadoop fs -put /executor_tars/spark-1.1.0-bin-2.3.0.tgz /
-
+echo "Executing /root/spark/bin/spark-submit --class WordCount3 --master mesos://zk://$ACTIVE_MASTER_PRIVATE:2181/mesos ~/test-code/simple-project_2.10-1.0.jar $ACTIVE_MASTER_PRIVATE"
 /root/spark/bin/spark-submit --class WordCount3 --master mesos://zk://$ACTIVE_MASTER_PRIVATE:2181/mesos ~/test-code/simple-project_2.10-1.0.jar $ACTIVE_MASTER_PRIVATE
 
 popd
