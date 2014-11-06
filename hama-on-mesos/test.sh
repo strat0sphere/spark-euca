64165f47a927e81a2d06c80ee0f0235ff38fae50
Stratos Dimopoulos <stratos.dimopoulos@gmail.com>
2014-11-05 17:19:46 -0800

    adding hama tests

---
+++ hama-on-mesos/test.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+
+pushd /root
+
+cd hama/dist/target/hama-0.7.0-SNAPSHOT/hama-0.7.0-SNAPSHOT
+
+echo "Executing: ./bin/hama jar hama-examples-0.7.0-SNAPSHOT.jar gen fastgen 100 10 randomgraph 2"
+./bin/hama jar hama-examples-0.7.0-SNAPSHOT.jar gen fastgen 100 10 randomgraph 2
+
+echo "HDFS should list /pageranksresult..."
+hadoop fs -ls /user/root
+
+echo "Executing: ./bin/hama jar hama-examples-0.7.0-SNAPSHOT.jar pagerank randomgraph pagerankresult 4"
+./bin/hama jar hama-examples-0.7.0-SNAPSHOT.jar pagerank randomgraph pagerankresult 4
+
+echo "HDFS should list /randomgraph directories..."
+hadoop fs -ls /user/root
+
+popd /root
