03f0e691a569f880f316c98383cfffb83da4ebad
Stratos Dimopoulos <stratos.dimopoulos@gmail.com>
2014-11-06 17:27:13 -0800

    improving test

--- backup/test.sh
+++ backup/test.sh
@@ -1,5 +1,12 @@
 #!bin/bash
 
+echo "Putting file on backup folder"
+mkdir -p /mnt/hdfs-backup
+touch /mnt/hdfs-backup/test.txt
+
 echo "Backup dry run: 's3cmd -c /etc/s3cmd/s3cfg  --dry-run --disable-multipart --delete-removed --delete-after --skip-existing sync /mnt/hdfs-backup s3://$CLUSTER_NAME/'"
 
 s3cmd -c /etc/s3cmd/s3cfg --dry-run --disable-multipart --delete-removed --delete-after --skip-existing sync /mnt/hdfs-backup s3://$CLUSTER_NAME/
+
+echo "Cleaning up..."
+rm -rf /mnt/hdfs-backup
