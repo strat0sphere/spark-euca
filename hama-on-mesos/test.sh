8fcc87a1347db2ccae4de38ba3dfe8c432bc20aa
Stratos Dimopoulos <stratos.dimopoulos@gmail.com>
2014-11-06 19:07:28 -0800

    fixing sha-bang

--- hama-on-mesos/test.sh
+++ hama-on-mesos/test.sh
@@ -1,9 +1,10 @@
-#/bin/bash
+#!/bin/bash
 
 pushd /root
 
 cd hama/
 echo "Executing: ./bin/hama jar hama-examples-0.7.0-SNAPSHOT.jar gen fastgen 100 10 randomgraph 2"
+
 ./bin/hama jar hama-examples-0.7.0-SNAPSHOT.jar gen fastgen 100 10 randomgraph 2
 
 echo "HDFS should list /pageranksresult..."
