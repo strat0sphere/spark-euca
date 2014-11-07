d55fa7831f914680d7e31ce8f59ee4486efe8edc
Stratos Dimopoulos <stratos.dimopoulos@gmail.com>
2014-11-06 17:35:28 -0800

    todo

--- hadoop-on-mesos/test.sh
+++ hadoop-on-mesos/test.sh
@@ -1,5 +1,6 @@
 #/bin/bash
 
+#TODO: FIX java.lang.NoClassDefFoundError: org/apache/hadoop/mapred/JobConf
 echo "I love UCSB" > /tmp/file0
 echo "Do you love UCSB?" > /tmp/file1
 chown hdfs:hadoop /tmp/file0
