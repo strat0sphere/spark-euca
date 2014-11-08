c5a845d047669b0162d15eb1c055d024f3fde178
Stratos Dimopoulos <stratos.dimopoulos@gmail.com>
2014-11-07 19:41:58 -0800

    bug fixes

--- hadoop-on-mesos/test.sh
+++ hadoop-on-mesos/test.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-echo "Printing environment variables:"
-env
-#TODO: FIX java.lang.NoClassDefFoundError: org/apache/hadoop/mapred/JobConf
+
+#Necessary to export HADOOP_MAPRED_HOME since the ssh session of setting it is not finished yet. Otherwise java.lang.NoClassDefFoundError: org/apache/hadoop/mapred/JobConf error will ne thrown
+export HADOOP_MAPRED_HOME=/usr/lib/hadoop-0.20-mapreduce
+
 echo "I love UCSB" > /tmp/file0
 echo "Do you love UCSB?" > /tmp/file1
 chown hdfs:hadoop /tmp/file0
--- hama-on-mesos/startup.sh
+++ hama-on-mesos/startup.sh
@@ -4,9 +4,11 @@ pushd /root
 
 cd hama
 
-echo "DEBUG: Print env variables"
-env
+#Necessary to export LD_LIBRARY_PATH since the ssh session of setting it is on /etc/enf is not finished yet. Otherwise the following error occurs: java.io.IOException: Call to euca-10-2-112-26.eucalyptus.internal/10.2.112.26:40000 failed on local exception: java.io.IOException: Connection reset by peer
+
 export LD_LIBRARY_PATH=/root/mesos-installation/lib/
+
+
 ./bin/hama-daemon.sh start bspmaster
 
 echo "HOSTNAME=$HOSTNAME"
