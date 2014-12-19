fb98abed3d0c37bfcd23f1ee6af618c8af1e4f38
Stratos Dimopoulos <stratos.dimopoulos@gmail.com>
2014-12-18 20:13:08 -0800

    comenting out streaming examples cause they run forever

--- spark-on-mesos/test.sh
+++ spark-on-mesos/test.sh
@@ -18,17 +18,17 @@ echo "Executing /root/spark/bin/spark-submit --class WordCount3 --master mesos:/
 /root/spark/bin/spark-submit --class WordCount3 --master mesos://zk://$ACTIVE_MASTER_PRIVATE:2181/mesos ~/test-code/simple-project-assembly_2.10-1.0.jar $ACTIVE_MASTER_PRIVATE
 
 #Requires Kafka to be already installed
-echo "Testing spark streaming with Kafka..."
+#echo "Testing spark streaming with Kafka..."
 
-echo "Starting producer..."
-echo "Executing /root/spark/bin/spark-submit --class testingsparkwithscala.KafkaWoCountProducer --master mesos://zk://$ACTIVE_MASTER_PRIVATE:2181/mesos ~/test-code/simple-project-assembly_2.10-1.0.jar $ACTIVE_MASTER_PRIVATE:9092 test-sentence 2 10"
+#echo "Starting producer..."
+#echo "Executing /root/spark/bin/spark-submit --class testingsparkwithscala.KafkaWoCountProducer --master mesos://zk://$ACTIVE_MASTER_PRIVATE:2181/mesos ~/test-code/simple-project-assembly_2.10-1.0.jar $ACTIVE_MASTER_PRIVATE:9092 test-sentence 2 10"
 
-nohup /root/spark/bin/spark-submit --class testingsparkwithscala.KafkaWoCountProducer --master mesos://zk://$ACTIVE_MASTER_PRIVATE:2181/mesos ~/test-code/simple-project-assembly_2.10-1.0.jar $ACTIVE_MASTER_PRIVATE:9092 test-sentence 2 10 &
+#nohup /root/spark/bin/spark-submit --class testingsparkwithscala.KafkaWoCountProducer --master mesos://zk://$ACTIVE_MASTER_PRIVATE:2181/mesos ~/test-code/simple-project-assembly_2.10-1.0.jar $ACTIVE_MASTER_PRIVATE:9092 test-sentence 2 10 &
 
 
-echo "Starting consumer..."
-echo "Executing /root/spark/bin/spark-submit --class testingsparkwithscala.KafkaWordCount --master mesos://zk://$ACTIVE_MASTER_PRIVATE:2181/mesos ~/test-code/simple-project-assembly_2.10-1.0.jar $ACTIVE_MASTER_PRIVATE spark-group test-sentence 1"
+#echo "Starting consumer..."
+#echo "Executing /root/spark/bin/spark-submit --class testingsparkwithscala.KafkaWordCount --master mesos://zk://$ACTIVE_MASTER_PRIVATE:2181/mesos ~/test-code/simple-project-assembly_2.10-1.0.jar $ACTIVE_MASTER_PRIVATE spark-group test-sentence 1"
 
-/root/spark/bin/spark-submit --class testingsparkwithscala.KafkaWordCount --master mesos://zk://$ACTIVE_MASTER_PRIVATE:2181/mesos ~/test-code/simple-project-assembly_2.10-1.0.jar $ACTIVE_MASTER_PRIVATE spark-group test-sentence 1
+#/root/spark/bin/spark-submit --class testingsparkwithscala.KafkaWordCount --master mesos://zk://$ACTIVE_MASTER_PRIVATE:2181/mesos ~/test-code/simple-project-assembly_2.10-1.0.jar $ACTIVE_MASTER_PRIVATE spark-group test-sentence 1
 
 popd
