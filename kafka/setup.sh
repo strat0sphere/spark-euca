1103352ae87eca52190b15e76be11599e18bdb9f
Stratos Dimopoulos <stratos.dimopoulos@gmail.com>
2014-12-18 16:51:43 -0800

    corrections

--- kafka/setup.sh
+++ kafka/setup.sh
@@ -4,7 +4,7 @@ tar -xzf kafka_2.9.2-0.8.1.1.tgz
 cd kafka_2.9.2-0.8.1.1
 #bin/kafka-server-start.sh config/server.properties
 
-ln -s /root/kafka-on-mesos/kafka-server-start.sh kafka-start
+ln -s /root/kafka/kafka-server-start.sh kafka-start
 update-rc.d kafka-start defaults
 
 popd
--- storm-on-mesos/setup.sh
+++ storm-on-mesos/setup.sh
@@ -6,8 +6,8 @@ cd storm-mesos-git/
 
 bin/build-release.sh downloadStormRelease
 STORM_RELEASE=`grep -1 -A 0 -B 0 '<version>' pom.xml | head -n 1 | awk '{print $1}' | sed -e 's/.*<version>//' | sed -e 's/<\/version>.*//'`
-bin/build-release.sh apache-storm-${RELEASE}.zip
-hadoop fs -put storm-mesos-${RELEASE}.tgz /
+bin/build-release.sh apache-storm-${STORM_RELEASE}.zip
+hadoop fs -put storm-mesos-${STORM_RELEASE}.tgz /
 cp storm-mesos-${STORM_RELEASE}.tgz /root/
 cd storm-mesos-${STORM_RELEASE}
 
