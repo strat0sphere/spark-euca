76536cd5cb645e701fe4c78dead74528f8616e1b
Stratos Dimopoulos <stratos.dimopoulos@gmail.com>
2014-12-18 18:06:00 -0800

    corrections

--- kafka/kafka-server-start.sh
+++ kafka/kafka-server-start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-/root/kafka_2.9.2-0.8.1.1/bin/kafka-server-start.sh config/server.properties
+pushd /root/kafka_2.9.2-0.8.1.1/
+
+./bin/kafka-server-start.sh config/server.properties
+
+popd
--- kafka/setup.sh
+++ kafka/setup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+
 pushd /root
 tar -xzf kafka_2.9.2-0.8.1.1.tgz
 cd kafka_2.9.2-0.8.1.1
 #bin/kafka-server-start.sh config/server.properties
 
-ln -s /root/kafka/kafka-server-start.sh kafka-start
+#add command to init.d
+ln -s /root/kafka_2.9.2-0.8.1.1/bin/kafka-server-start.sh kafka-start
+mv kafka-start /etc/init.d
 update-rc.d kafka-start defaults
 
 popd
