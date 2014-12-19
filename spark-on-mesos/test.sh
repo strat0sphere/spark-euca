a3af3a584e9c0188881728b7c90f4d7385a573cb
Stratos Dimopoulos <stratos.dimopoulos@gmail.com>
2014-12-18 17:05:21 -0800

    spark streaming tests added

--- spark-on-mesos/test.sh
+++ spark-on-mesos/test.sh
@@ -16,4 +16,18 @@ wget -P /root/test-code http://php.cs.ucsb.edu/spark-related-packages/test-code/
 echo "Executing /root/spark/bin/spark-submit --class WordCount3 --master mesos://zk://$ACTIVE_MASTER_PRIVATE:2181/mesos ~/test-code/simple-project_2.10-1.0.jar $ACTIVE_MASTER_PRIVATE"
 /root/spark/bin/spark-submit --class WordCount3 --master mesos://zk://$ACTIVE_MASTER_PRIVATE:2181/mesos ~/test-code/simple-project_2.10-1.0.jar $ACTIVE_MASTER_PRIVATE
 
+#Requires Kafka to be already installed
+echo "Testing spark streaming with Kafka..."
+
+echo "Starting producer..."
+echo "Executing /root/spark/bin/spark-submit --class testingsparkwithscala.KafkaWoCountProducer --master mesos://zk://$ACTIVE_MASTER_PRIVATE:2181/mesos ~/test-code/simple-project-assembly_2.10-1.0.jar $ACTIVE_MASTER_PRIVATE:9092 test-sentence 2 10"
+
+nohup /root/spark/bin/spark-submit --class testingsparkwithscala.KafkaWoCountProducer --master mesos://zk://$ACTIVE_MASTER_PRIVATE:2181/mesos ~/test-code/simple-project-assembly_2.10-1.0.jar $ACTIVE_MASTER_PRIVATE:9092 test-sentence 2 10 &
+
+
+echo "Starting consumer..."
+echo "Executing /root/spark/bin/spark-submit --class testingsparkwithscala.KafkaWordCount --master mesos://zk://$ACTIVE_MASTER_PRIVATE:2181/mesos ~/test-code/simple-project-assembly_2.10-1.0.jar $ACTIVE_MASTER_PRIVATE spark-group test-sentence 1"
+
+/root/spark/bin/spark-submit --class testingsparkwithscala.KafkaWordCount --master mesos://zk://$ACTIVE_MASTER_PRIVATE:2181/mesos ~/test-code/simple-project-assembly_2.10-1.0.jar $ACTIVE_MASTER_PRIVATE spark-group test-sentence 1
+
 popd
