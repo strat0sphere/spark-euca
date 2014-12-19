413252bd44f5c03c5f762aa0b2bb35742f18a6ae
Stratos Dimopoulos <stratos.dimopoulos@gmail.com>
2014-12-18 19:05:19 -0800

    getting test code from s3

--- spark-on-mesos/test.sh
+++ spark-on-mesos/test.sh
@@ -11,10 +11,11 @@ hadoop fs -put /tmp/file0 /user/foo/data
 
 echo "Getting sample test code from server..."
 rm /root/test-code/simple-project_2.10-1.0.jar
-wget -P /root/test-code http://php.cs.ucsb.edu/spark-related-packages/test-code/simple-project_2.10-1.0.jar
+s3cmd -c /etc/s3cmd/s3cfg get --recursive --disable-multipart s3://mesos-repo/test-code/simple-project-assembly_2.10-1.0.jar /root/test-code
+#wget -P /root/test-code http://php.cs.ucsb.edu/spark-related-packages/test-code/simple-project_2.10-1.0.jar
 
 echo "Executing /root/spark/bin/spark-submit --class WordCount3 --master mesos://zk://$ACTIVE_MASTER_PRIVATE:2181/mesos ~/test-code/simple-project_2.10-1.0.jar $ACTIVE_MASTER_PRIVATE"
-/root/spark/bin/spark-submit --class WordCount3 --master mesos://zk://$ACTIVE_MASTER_PRIVATE:2181/mesos ~/test-code/simple-project_2.10-1.0.jar $ACTIVE_MASTER_PRIVATE
+/root/spark/bin/spark-submit --class WordCount3 --master mesos://zk://$ACTIVE_MASTER_PRIVATE:2181/mesos ~/test-code/simple-project-assembly_2.10-1.0.jar $ACTIVE_MASTER_PRIVATE
 
 #Requires Kafka to be already installed
 echo "Testing spark streaming with Kafka..."
