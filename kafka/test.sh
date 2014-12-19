1b563ff90c409eb4e77a19f01c27e857ac3226af
Stratos Dimopoulos <stratos.dimopoulos@gmail.com>
2014-12-18 17:05:00 -0800

    comments added

--- kafka/test.sh
+++ kafka/test.sh
@@ -3,7 +3,11 @@
 pushd root
 
 cd kafka_2.9.2-0.8.1.1
-./bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partition 1 --topic test
-./bin/kafka-topics.sh --list --zookeeper localhost:2181
+echo "Creating Kafka topic..."
+echo "Executing ./bin/kafka-topics.sh --create --zookeeper $ACTIVE_MASTER_PRIVATE:2181 --replication-factor 1 --partition 1 --topic test"
+./bin/kafka-topics.sh --create --zookeeper $ACTIVE_MASTER_PRIVATE:2181 --replication-factor 1 --partition 1 --topic test
+
+echo "Executing ./bin/kafka-topics.sh --list --zookeeper $ACTIVE_MASTER_PRIVATE:2181"
+./bin/kafka-topics.sh --list --zookeeper $ACTIVE_MASTER_PRIVATE:2181
 
 popd
