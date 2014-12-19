c7e6ef2a0c28470a06edb9127b9830688fe5d5da
Stratos Dimopoulos <stratos.dimopoulos@gmail.com>
2014-12-18 18:19:27 -0800

    fixes

--- kafka/setup.sh
+++ kafka/setup.sh
@@ -6,8 +6,7 @@ cd kafka_2.9.2-0.8.1.1
 #bin/kafka-server-start.sh config/server.properties
 
 #add command to init.d
-ln -s /root/kafka_2.9.2-0.8.1.1/bin/kafka-server-start.sh kafka-start
-mv kafka-start /etc/init.d
+ln -s /root/kafka_2.9.2-0.8.1.1/bin/kafka-server-start.sh /etc/init.d/kafka-start
 update-rc.d kafka-start defaults
 
 popd
--- storm-on-mesos/setup.sh
+++ storm-on-mesos/setup.sh
@@ -9,7 +9,9 @@ STORM_RELEASE=`grep -1 -A 0 -B 0 '<version>' pom.xml | head -n 1 | awk '{print $
 bin/build-release.sh apache-storm-${STORM_RELEASE}.zip
 hadoop fs -put storm-mesos-${STORM_RELEASE}.tgz /
 cp storm-mesos-${STORM_RELEASE}.tgz /root/
-cd storm-mesos-${STORM_RELEASE}
+cd /root
+tar xzf storm-mesos-${STORM_RELEASE}.tgz
+cd /root/storm-mesos-${STORM_RELEASE}
 
 #copy configuration template
 cp /etc/storm/storm.yaml ./conf
@@ -19,12 +21,12 @@ cp /etc/storm/logback ./logback
 #nohup bin/storm-mesos ui  > ui.out &
 
 #Adding soft links to automatically start services on reboot
-cp /root/spark-euca/storm-on-mesos/start-nimbus.sh storm-mesos-${STORM_RELEASE}/bin
-ln -s /root/storm-mesos-${STORM_RELEASE}/bin/start-nimbus.sh storm-nimbus-start
+cp /root/spark-euca/storm-on-mesos/start-nimbus.sh bin/
+ln -s /root/storm-mesos-${STORM_RELEASE}/bin/start-nimbus.sh /etc/init.d/storm-nimbus-start
 update-rc.d storm-nimbus-start defaults
 
-cp /root/spark-euca/storm-on-mesos/start-storm-ui.sh storm-mesos-${STORM_RELEASE}/bin
-ln -s /root/storm-mesos-${STORM_RELEASE}/bin/start-storm-ui.sh storm-ui-start
+cp /root/spark-euca/storm-on-mesos/start-storm-ui.sh bin/
+ln -s /root/storm-mesos-${STORM_RELEASE}/bin/start-storm-ui.sh /etc/init.d/storm-ui-start
 update-rc.d storm-ui-start defaults
 
 popd
