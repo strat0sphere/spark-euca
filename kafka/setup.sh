fe03ceedc2edc8379343164b6c5f9c3868bd9aeb
Stratos Dimopoulos <stratos.dimopoulos@gmail.com>
2014-12-20 15:15:07 -0800

    leaving external startup to etc

--- kafka/setup.sh
+++ kafka/setup.sh
@@ -6,11 +6,10 @@ rm kafka_${KAFKA_SCALA_BINARY}.tgz
 cd kafka_${KAFKA_SCALA_BINARY}
 
 cp /etc/kafka/config/server.properties ./config/
-cp /etc/kafka/start-kafka.sh ./bin/
 
 #add command to init.d
-chmod +x ./bin/start-kafka.sh
-ln -s /root/kafka_${KAFKA_SCALA_BINARY}/bin/start-kafka.sh /etc/init.d/kafka-start
+chmod +x /etc/kafka/start-kafka.sh
+ln -s /etc/kafka/start-kafka.sh /etc/init.d/kafka-start
 update-rc.d kafka-start defaults
 
 popd
