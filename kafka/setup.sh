e300b67f1ec3d8b0d6338dafe70fbe74176ad913
Stratos Dimopoulos <stratos.dimopoulos@gmail.com>
2014-12-20 13:02:01 -0800

    bug fix

--- kafka/setup.sh
+++ kafka/setup.sh
@@ -5,8 +5,8 @@ tar -xzf kafka_2.9.2-0.8.1.1.tgz
 rm kafka_2.9.2-0.8.1.1.tgz
 cd kafka_2.9.2-0.8.1.1
 
-cp /etc/kafka/config/server/properties ./config/
-cp /etc/start-kafka.sh ./bin/
+cp /etc/kafka/config/server.properties ./config/
+cp /etc/kafka/start-kafka.sh ./bin/
 
 #add command to init.d
 chmod +x ./bin/start-kafka.sh
