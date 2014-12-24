c0050dd2da2a448f963df9a88827accc0668608f
Stratos Dimopoulos <stratos.dimopoulos@gmail.com>
2014-12-23 17:25:27 -0800

    buf fix

--- kafka/setup.sh
+++ kafka/setup.sh
@@ -12,13 +12,16 @@ cd kafka
 cp /etc/kafka/config/server.properties ./config/
 
 #add command to init.d
-chmod +x kafka/kafka-server.sh
-ln -s /root/kafka/kafka-server.sh /etc/init.d/kafka-server
+cp /root/spark-euca/kafka/kafka-server.sh ./bin
+cp /root/spark-euca/kafka/kafka-restart.sh ./bin
+
+chmod +x bin/kafka-server.sh
+ln -s /root/kafka/bin/kafka-server.sh /etc/init.d/kafka-server
 update-rc.d kafka-server defaults
 
 #add restart command to init.d with lower priority
-chmod +x kafka/kafka-restart.sh
-ln -s /root/kafka/kafka-restart.sh /etc/init.d/kafka-restart
+chmod +x bin/kafka-restart.sh
+ln -s /root/kafka/bin/kafka-restart.sh /etc/init.d/kafka-restart
 update-rc.d kafka-restart defaults 91
 
 #creating log dir
