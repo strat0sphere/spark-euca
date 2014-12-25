2178c220d485c2b7052d835efacf6f6646134d90
Stratos Dimopoulos <stratos.dimopoulos@gmail.com>
2014-12-25 11:54:08 -0800

    storm-kafka starting issue fix

--- templates/etc/storm/start-nimbus.sh
+++ templates/etc/storm/start-nimbus.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nohup /root/storm-mesos-{{storm_release}}/bin/storm-mesos nimbus > /mnt/storm-logs/nimbus.out 2>&1 &
+/root/storm-mesos-{{storm_release}}/bin/storm-mesos nimbus > /mnt/storm-logs/nimbus.out 2>&1 &
--- templates/etc/storm/start-storm-ui.sh
+++ templates/etc/storm/start-storm-ui.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nohup /root/storm-mesos-{{storm_release}}/bin/storm ui > /mnt/storm-logs/ui.out 2>&1 &
+/root/storm-mesos-{{storm_release}}/bin/storm ui > /mnt/storm-logs/ui.out 2>&1 &
