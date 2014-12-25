d76b29e0db7dd7ac421456aec4388dc0635a7855
Stratos Dimopoulos <stratos.dimopoulos@gmail.com>
2014-12-24 21:25:41 -0800

    printing nimbus output

--- storm-on-mesos/test.sh
+++ storm-on-mesos/test.sh
@@ -7,6 +7,7 @@ cd storm-mesos-${STORM_RELEASE}
 ./bin/storm jar ../storm-starter-0.0.1-SNAPSHOT.jar storm.starter.WordCountTopology WordCount
 sleep 30.0
 #Print sth to prove its working
+tail /mnt/storm-logs/nimbus.log
 ./bin/storm kill WordCount
 
 popd
