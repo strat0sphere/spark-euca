9ff929e92e159b04b440752c0c4c621d355419dc
Stratos Dimopoulos <stratos.dimopoulos@gmail.com>
2014-12-24 20:26:28 -0800

    no longer clone repo from branch

--- euca/mesos_euca_emi.py
+++ euca/mesos_euca_emi.py
@@ -448,7 +448,7 @@ def setup_cluster(conn, master_nodes, slave_nodes, zoo_nodes, opts, deploy_ssh_k
   # "hama-on-mesos"
   modules = ["s3cmd", "spark-on-mesos", "hadoop-on-mesos", "backup", "storm-on-mesos", "kafka" ] #It is also defined on deploy_templates_mesos
 
-  ssh(master, opts, "rm -rf spark-euca && git clone -b mesos-emi https://github.com/strat0sphere/spark-euca.git")
+  ssh(master, opts, "rm -rf spark-euca && git clone https://github.com/strat0sphere/spark-euca.git")
 
   print "Deploying files to master..."
   deploy_files(conn, "deploy.mesos-emi", opts, master_nodes, slave_nodes, zoo_nodes, modules, s3conn)
