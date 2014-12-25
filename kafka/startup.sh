fdea7c1a59d8a82458aa0bf2400d5829b002d621
Stratos Dimopoulos <stratos.dimopoulos@gmail.com>
2014-12-24 17:06:18 -0800

    debugging

--- kafka/startup.sh
+++ kafka/startup.sh
@@ -1,17 +1,19 @@
 #/bin/bash
 
 #start-up script at /etc/kafka
-echo "DEBUG: Starting kafka..."
-service kafka-server start
-echo "DEBUG: Sleeping..."
-sleep 10
-tail /mnt/kafka-logs/kafka.out
+#echo "DEBUG: Starting kafka..."
+#service kafka-server start
+#echo "DEBUG: Sleeping..."
+#sleep 10
+#tail /mnt/kafka-logs/kafka.out
 
-ps -ef | grep kafka
+#ps -ef | grep kafka
 
 echo "DEBUG: Starting kafka running: /root/kafka/bin/kafka-server-start.sh /root/kafka/config/server.properties > /mnt/kafka-logs/kafka.out 2>&1 & ..."
 
 /root/kafka/bin/kafka-server-start.sh /root/kafka/config/server.properties > /mnt/kafka-logs/kafka.out 2>&1 &
+
+sleep 10
 
 tail /mnt/kafka-logs/kafka.out
 
