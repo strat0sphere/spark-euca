016bc423e18b3c814caa3d50b768d06f83a29669
Stratos Dimopoulos <stratos.dimopoulos@gmail.com>
2014-12-24 16:12:31 -0800

    debugging

--- kafka/startup.sh
+++ kafka/startup.sh
@@ -2,6 +2,13 @@
 
 #start-up script at /etc/kafka
 service kafka-server start
+sleep 10
+tail /mnt/kafka-logs/kafka.out
+
+ps -ef | grep kafka
+
+/root/kafka/bin/kafka-server-start.sh /root/kafka/config/server.properties > /mnt/kafka-logs/kafka.out 2>&1 &
+
 tail /mnt/kafka-logs/kafka.out
 
 ps -ef | grep kafka
