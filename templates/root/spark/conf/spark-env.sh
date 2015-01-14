537b92f7e5af880f21d89676c1bbde832f7294db
Stratos Dimopoulos <stratos.dimopoulos@gmail.com>
2015-01-14 15:09:03 -0800

    preparing for spark HA

--- templates/root/spark/conf/spark-env.sh
+++ templates/root/spark/conf/spark-env.sh
@@ -9,6 +9,7 @@ export HADOOP_CONF_DIR=/etc/hadoop/conf.mesos-cluster/
 
 export HADOOP_HOME=/usr/lib/hadoop
 
+
 #Addressing worning for hadoop native library issue - None of the bellow work!
 #export SPARK_DAEMON_JAVA_OPTS=-Djava.library.path=$HADOOP_HOME/lib/native/
 #export SPARK_WORKER_OPTS=-Djava.library.path=$HADOOP_HOME/lib/native/
