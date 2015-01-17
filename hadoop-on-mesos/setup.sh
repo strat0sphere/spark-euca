a2982499ef3fd517e081e73aa406b89254a7f51a
Stratos Dimopoulos <stratos.dimopoulos@gmail.com>
2015-01-17 11:06:38 -0800

    adding HADOO_MAPRED_HOME env variable

--- hadoop-on-mesos/setup.sh
+++ hadoop-on-mesos/setup.sh
@@ -2,3 +2,4 @@
 
 #In the restore from emi version the setup is already done - file exists just for completence
 echo "Hadoop on Mesos executor already setup - nothing more to do..."
+echo "HADOOP_MAPRED_HOME=/usr/lib/hadoop-0.20-mapreduce/" >> /etc/environment
