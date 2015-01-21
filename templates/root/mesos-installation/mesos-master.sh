da6060b8a7b8c4fd39e4e56140f4e99656d1e185
Stratos Dimopoulos <stratos.dimopoulos@gmail.com>
2015-01-21 15:52:23 -0800

    check if process already runs on startup

--- templates/root/mesos-installation/mesos-master.sh
+++ templates/root/mesos-installation/mesos-master.sh
@@ -3,7 +3,7 @@
 # chkconfig: 345 80 20
 # description: mesos-master
 
-# pidfile: /var/run/mesos/mesos-master.pid
+# pidfile: $PID_FILE
 ### BEGIN INIT INFO
 # Provides:          mesos-master
 # Required-Start:    $network $local_fs
@@ -23,21 +23,44 @@ DAEMON_NAME=mesos-master
 
 PATH=$PATH:$DAEMON_PATH/bin
 
+PID_FILE=/var/run/mesos/mesos-master.pid
+
+# Check if a service is running
+isrunning() {
+ISRUNNING="0"
+# Do we have PID-file?
+if [ -f "$PIDDIR/$1.pid" ]; then
+# Check if proc is running
+pid=`cat "$PIDDIR/$1.pid" 2> /dev/null`
+if [ "$pid" != "" ]; then
+if [ -d /proc/$pid ]; then
+# Process is running
+ISRUNNING="1"
+fi
+fi
+fi
+#ISRUNNING="0"
+}
+
 case "$1" in
 start)
+if isrunning $PID_FILE ; then
+echo "Error: $DAEMON_NAME is running. Stop it first." >&2
+exit 1
+else
 # Start daemon.
 mkdir /var/run/mesos
 echo -n "Starting $DAEMON_NAME: ";echo
 nohup $DAEMON_PATH/sbin/mesos-master --cluster={{cluster_name}} --log_dir=/mnt/mesos-logs --zk={{cluster_url_private_ip}} --work_dir=/mnt/mesos-work-dir/ --quorum=1 start </dev/null >/dev/null 2>&1 &
-sleep 3.0
-ps ax | grep -i 'mesos-master' | grep -v grep | awk '{print $1}' > /var/run/mesos/mesos-master.pid
-#echo $(($$+1)) > /var/run/mesos/mesos-master.pid
+#sleep 3.0
+#ps ax | grep -i 'mesos-master' | grep -v grep | awk '{print $1}' > $PID_FILE
+echo $(($$+1)) > $PID_FILE
 ;;
 stop)
 # Stop daemons.
 echo -n "Shutting down $DAEMON_NAME: ";echo
-cat /var/run/mesos/mesos-master.pid | xargs kill -9
-rm -rf /var/run/mesos/mesos-master.pid
+cat $PID_FILE | xargs kill -9
+rm -rf $PID_FILE
 ;;
 restart)
 $0 stop
--- templates/root/mesos-installation/mesos-slave.sh
+++ templates/root/mesos-installation/mesos-slave.sh
@@ -3,7 +3,7 @@
 # chkconfig: 345 80 20
 # description: mesos-slave
 
-# pidfile: /var/run/mesos/mesos-slave.pid
+# pidfile: $PID_FILE
 ### BEGIN INIT INFO
 # Provides:          mesos-slave
 # Required-Start:    $network $local_fs
@@ -23,22 +23,45 @@ DAEMON_NAME=mesos-slave
 
 PATH=$PATH:$DAEMON_PATH/bin
 
+PID_FILE=/var/run/mesos/mesos-slave.pid
+
+# Check if a service is running
+isrunning() {
+ISRUNNING="0"
+# Do we have PID-file?
+if [ -f "$PIDDIR/$1.pid" ]; then
+# Check if proc is running
+pid=`cat "$PIDDIR/$1.pid" 2> /dev/null`
+if [ "$pid" != "" ]; then
+if [ -d /proc/$pid ]; then
+# Process is running
+ISRUNNING="1"
+fi
+fi
+fi
+#ISRUNNING="0"
+}
+
 case "$1" in
 start)
+if isrunning $PID_FILE ; then
+echo "Error: $DAEMON_NAME is running. Stop it first." >&2
+exit 1
+else
 # Start daemon.
 mkdir /var/run/mesos
 echo -n "Starting $DAEMON_NAME: ";echo
 
 nohup $DAEMON_PATH/sbin/mesos-slave --log_dir=/mnt/mesos-logs --work_dir=/mnt/mesos-work-dir/ --master={{cluster_url_private_ip}} </dev/null >/dev/null 2>&1 &
-sleep 3.0
-ps ax | grep -i 'mesos-slave' | grep -v grep | awk '{print $1}' > /var/run/mesos/mesos-slave.pid
-#echo $(($$+1)) > /var/run/mesos/mesos-slave.pid
+#sleep 3.0
+#ps ax | grep -i 'mesos-slave' | grep -v grep | awk '{print $1}' > $PID_FILE
+echo $(($$+1)) > $PID_FILE
 ;;
 stop)
 # Stop daemons.
 echo -n "Shutting down $DAEMON_NAME: ";echo
-cat /var/run/mesos/mesos-slave.pid | xargs kill -9
-rm -rf /var/run/mesos/mesos-slave.pid
+cat $PID_FILE | xargs kill -9
+rm -rf $PID_FILE
 ;;
 restart)
 $0 stop
