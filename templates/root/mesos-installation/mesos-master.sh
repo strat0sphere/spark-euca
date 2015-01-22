300ef1d254ca679d25adc9752f7bb04baab20458
Stratos Dimopoulos <stratos.dimopoulos@gmail.com>
2015-01-21 22:51:54 -0800

    oh no

--- templates/root/mesos-installation/mesos-master.sh
+++ templates/root/mesos-installation/mesos-master.sh
@@ -52,7 +52,7 @@ else
 # Start daemon.
 mkdir /var/run/mesos
 echo -n "Starting $DAEMON_NAME: ";echo
-nohup $DAEMON_PATH/sbin/mesos-master --cluster=ha --log_dir=/mnt/mesos-logs --zk=zk://10.2.246.253:2181,10.2.246.240:2181,10.2.246.247:2181/mesos --work_dir=/mnt/mesos-work-dir/ --quorum=1 start </dev/null >/dev/null 2>&1 &
+nohup $DAEMON_PATH/sbin/mesos-master --cluster={{cluster_name}} --log_dir=/mnt/mesos-logs --zk={{cluster_url_private_ip}} --work_dir=/mnt/mesos-work-dir/ --quorum=1 start </dev/null >/dev/null 2>&1 &
 sleep 3.0
 ps ax | grep -i 'sbin/mesos-master' | grep -v grep | awk '{print $1}' > $PID_FILE
 fi
