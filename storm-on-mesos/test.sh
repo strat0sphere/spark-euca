808c3380d6ec88c1657d50451fe55c81718ccdf4
Stratos Dimopoulos <stratos.dimopoulos@gmail.com>
2015-02-03 20:33:36 -0800

    no need to dowload storm starter

--- storm-on-mesos/test.sh
+++ storm-on-mesos/test.sh
@@ -2,9 +2,10 @@
 
 pushd /root
 
-wget http://downloads.mesosphere.io/storm/storm-starter-0.0.1-SNAPSHOT.jar
+#wget http://downloads.mesosphere.io/storm/storm-starter-0.0.1-SNAPSHOT.jar
 cd storm-mesos-${STORM_RELEASE}
-./bin/storm jar ../storm-starter-0.0.1-SNAPSHOT.jar storm.starter.WordCountTopology WordCount
+/root/storm-mesos-0.9.2-incubating/bin/storm jar /root/storm-mesos-0.9.2-incubating/examples/storm-starter/storm-starter-topologies-0.9.2-incubating.jar storm.starter.WordCountTopology WordCount
+#./bin/storm jar ../storm-starter-0.0.1-SNAPSHOT.jar storm.starter.WordCountTopology WordCount
 sleep 30.0
 #Print sth to prove its working
 tail /mnt/storm-logs/nimbus.log
