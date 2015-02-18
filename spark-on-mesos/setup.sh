e12fb83bb46b2da90ff64ecf4504957fd22fc24b
Stratos Dimopoulos <stratos.dimopoulos@gmail.com>
2015-02-18 01:00:47 -0800

    spark-submit command

--- spark-on-mesos/setup.sh
+++ spark-on-mesos/setup.sh
@@ -1,1 +1,7 @@
 #/bin/bash
+
+#Create spark-submit command
+echo '#!/bin/bash' >> /usr/bin/spark-submit
+echo 'exec /root/spark/bin/spark-submit "$@"' >> /usr/bin/spark-submit
+
+chmod 755 /usr/bin/spark-submit
