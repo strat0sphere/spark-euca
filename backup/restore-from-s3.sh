060f0c587368bf7726a9e1756eacccef14b2c53f
Stratos Dimopoulos <stratos.dimopoulos@gmail.com>
2015-02-24 20:19:55 -0800

    improving debug messages

--- backup/restore-from-s3.sh
+++ backup/restore-from-s3.sh
@@ -5,9 +5,11 @@ mkdir -p /mnt/hdfs-backup
 chown -R hdfs:hadoop /mnt/hdfs-backup
 
 date >> /mnt/hdfs-backup-logs/restore.log
-echo "Geting files from S3..." | tee -a /mnt/hdfs-backup-logs/restore.log 1>&2
+echo "Geting files from S3 bucket $CLUSTER_NAME/..." | tee -a /mnt/hdfs-backup-logs/restore.log 1>&2
 #Get the file from S3
-s3cmd -c /etc/s3cmd/s3cfg get --recursive --disable-multipart s3://$CLUSTER_NAME/hdfs-backup/ /mnt/hdfs-backup/
+s3cmd -c /etc/s3cmd/s3cfg get --recursive --disable-multipart s3://$CLUSTER_NAME/hdfs-backup/ /mnt/hdfs-backup/ 2>> /mnt/hdfs-backup-logs/restore.log
+
+tail /mnt/hdfs-backup-logs/restore.log
 echo "Done!"
 
 echo "Putting files on HDFS..." | tee -a /mnt/hdfs-backup-logs/restore.log 1>&2
