c01c1d9381a0edf033e1b97e6132afacdda5cd41
Stratos Dimopoulos <stratos.dimopoulos@gmail.com>
2015-09-28 11:37:18 -0700

    changing version

--- spark-on-mesos/init.sh
+++ spark-on-mesos/init.sh
@@ -3,17 +3,16 @@
 pushd /root
 
 mkdir /executor_tars
-s3cmd -c /etc/s3cmd/s3cfg get --recursive --disable-multipart s3://mesos-repo/executors/spark-1.1.0-bin-2.3.0.tgz /executor_tars/
-#wget -P /executor_tars http://php.cs.ucsb.edu/spark-related-packages/executor_tars/spark-1.1.0-bin-2.3.0.tgz
+s3cmd -c /etc/s3cmd/s3cfg get --recursive --disable-multipart s3://mesos-repo/executors/spark-1.2.1-bin-2.3.0-mr1-cdh5.1.2.tgz /executor_tars/
 
 chown -R hdfs:hadoop /executor_tars
 
-echo "Putting spark-1.1.0-bin-2.3.0.tgz to HDFS..."
+echo "Putting spark-1.2.1-bin-2.3.0-mr1-cdh5.1.2..tgz to HDFS..."
 #/executor_tars directory already exists on the emi
-hadoop fs -put /executor_tars/spark-1.1.0-bin-2.3.0.tgz /
+hadoop fs -put /executor_tars/spark-1.2.1-bin-2.3.0-mr1-cdh5.1.2.tgz /
 hadoop fs -ls /
 
 #delete to save some space if necessary
-rm /executor_tars/spark-1.1.0-bin-2.3.0.tgz
+rm /executor_tars/spark-1.2.1-bin-2.3.0-mr1-cdh5.1.2.tgz
 
 popd
