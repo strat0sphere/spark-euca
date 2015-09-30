83c66331ba66f4bb70783c6f4b08e5fa46f5e2fd
Stratos Dimopoulos <stratos.dimopoulos@gmail.com>
2015-09-30 14:43:08 -0700

    keep previous config

--- cloudera-hdfs/init.sh
+++ cloudera-hdfs/init.sh
@@ -6,4 +6,4 @@ update-alternatives --set hadoop-conf /etc/hadoop/conf.mesos-cluster
 wget http://archive.cloudera.com/cdh5/one-click-install/precise/amd64/cdh5-repository_1.0_all.deb
 
 echo "Running dpkg --force-confdef -i cdh5-repository_1.0_all.deb"
-dpkg -i cdh5-repository_1.0_all.deb
+dpkg --force-confdef -i cdh5-repository_1.0_all.deb
