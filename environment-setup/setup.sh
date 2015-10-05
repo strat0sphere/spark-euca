95a9408887387e15c52ee5ada04bef67c902e3ea
Stratos Dimopoulos <stratos.dimopoulos@gmail.com>
2015-10-04 19:35:13 -0700

    more printing

--- environment-setup/setup.sh
+++ environment-setup/setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-apt-get -qq update
-apt-get -qq --yes --force-yes install git
+apt-get -q update
+apt-get -q --yes --force-yes install git
 
-apt-get -qq --yes --force-yes install openjdk-7-jdk
+apt-get -q --yes --force-yes install openjdk-7-jdk
 mv /usr/lib/jvm/java-7-openjdk-amd64 /usr/lib/jvm/java-1.7.0
 update-alternatives --install /usr/bin/java java /usr/lib/jvm/java-1.7.0/jre/bin/java 2
 update-alternatives --install /usr/bin/javac javac /usr/lib/jvm/java-1.7.0/bin/javac 2
 
-apt-get -qq --yes --force-yes install openjdk-6-jdk
+apt-get -q --yes --force-yes install openjdk-6-jdk
 mv /usr/lib/jvm/java-6-openjdk-amd64 /usr/lib/jvm/java-1.6.0
 update-alternatives --install /usr/bin/java java /usr/lib/jvm/java-1.6.0/jre/bin/java 1
 update-alternatives --install /usr/bin/javac javac /usr/lib/jvm/java-1.6.0/bin/javac 2
@@ -17,6 +17,6 @@ tar xvf scala-2.11.1.tgz
 mv scala-2.11.1 scala
 rm scala-2.11.1.tgz
 
-apt-get -qq --yes --force-yes install libgfortran3
+apt-get -q --yes --force-yes install libgfortran3
 
 
