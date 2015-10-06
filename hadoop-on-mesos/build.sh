c1be81eaad66bdf158eed10d793fee7d288c68bd
Stratos Dimopoulos <stratos.dimopoulos@gmail.com>
2015-10-06 13:24:53 -0700

    descriptive text

--- hadoop-on-mesos/build.sh
+++ hadoop-on-mesos/build.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+echo "Making sure maven is installed..."
 apt-get -qq --yes --force-yes install maven
 
+echo "Removing hadoop-2.3.0-cdh5.1.2.tar.gz if exists..."
 rm hadoop-2.3.0-cdh5.1.2.tar.gz
+
+echo "Getting hadoop-2.3.0-cdh5.1.2.tar.gz..."
 wget http://archive.cloudera.com/cdh5/cdh/5/hadoop-2.3.0-cdh5.1.2.tar.gz
+
+echo "Untarring hadoop-2.3.0-cdh5.1.2.tar.gz..."
 tar zxf hadoop-2.3.0-cdh5.1.2.tar.gz
 
 echo "Cloning Hadoop-on-mesos..."
@@ -14,7 +20,7 @@ cd /root/hadoopOnMesos
 echo "Running mvn package..."
 mvn package
 cp target/hadoop-mesos-0.0.8.jar /usr/lib/hadoop-0.20-mapreduce/lib/
-cp target/hadoop-mesos-0.0.8.jar hadoop-2.3.0-cdh5.1.2/share/hadoop/common/lib/
+cp target/hadoop-mesos-0.0.8.jar /root/hadoop-2.3.0-cdh5.1.2/share/hadoop/common/lib/
 
 cd /root/hadoop-2.3.0-cdh5.1.2
 
@@ -39,6 +45,11 @@ cd /root #make sure you are on root dir
 #pack to be ready to upload to HDFS
 echo "Packing hadoop-on-mesos..."
 tar czf hadoop-2.3.0-cdh5.1.2-mesos.0.21.1.tar.gz hadoop-2.3.0-cdh5.1.2
+
+echo "Moving hadoop-2.3.0-cdh5.1.2-mesos.0.21.1.tar.gz to /executor_tars"
 mv hadoop-2.3.0-cdh5.1.2-mesos.0.21.1.tar.gz /executor_tars
+
+echo "Listing executor_tars files:"
+ls /executor_tars
 
 
